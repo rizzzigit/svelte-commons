8cef1ec51f47d561f66ed381225bc7f2dfdda7c2
Rizzzi Git <rizzzigit@gmail.com>
2024-06-07 18:29:42 +0800

    import css properly

--- src/lib/reset-css.svelte
+++ src/lib/reset-css.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import 'reset-css'
+  import 'reset-css/reset.css'
 </script>
 
 <!-- svelte-ignore css_unused_selector -->
 <style lang="scss">
-  @import 'reset-css';
-
   :global(html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video) {
     font: revert;
   }
